f80e389317bf81f0e21716ba4e5d1665407e8656
SampathAbhishek <62943320+SampathAbhishek@users.noreply.github.com>
2021-10-24 15:14:42 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+alert("Welcome to the love calculator");
+var name1 = prompt("Enter lover1 name");
+var name2 = prompt("Enter lover2 name");
+n=100;
+alert(name1+" and "+name2+" has a love of "+Math.floor(n*Math.random())+"% love according to love calculator");
